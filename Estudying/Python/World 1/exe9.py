07118e5588b78bfe9fb72cb074e490e3af2204ae
mac <85289658+85406043@users.noreply.github.com>
2026-01-28 11:09:29 -0300

    Create exe9.py

---
+++ Estudying/Python/World 1/exe9.py
@@ -0,0 +1,24 @@
+#I need an initial number.
+number = int(input("Enter a number: "))
+#Calculation numbers
+sum_1 = number * 1
+sum_2 = number * 2
+sum_3 = number * 3
+sum_4 = number * 4
+sum_5 = number * 5
+sum_6 = number * 6
+sum_7 = number * 7
+sum_8 = number * 8
+sum_9 = number * 9
+sum_10 = number * 10
+#Show multiplication table
+print("{} x  1 = {}".format(number, sum_1))# print("{} x {:2} = {}".format(number, 1, sum_1))
+print("{} x  2 = {}".format(number, sum_2))
+print("{} x  3 = {}".format(number, sum_3))
+print("{} x  4 = {}".format(number, sum_4))
+print("{} x  5 = {}".format(number, sum_5))
+print("{} x  6 = {}".format(number, sum_6))
+print("{} x  7 = {}".format(number, sum_7))
+print("{} x  8 = {}".format(number, sum_8))
+print("{} x  9 = {}".format(number, sum_9))
+print("{} x 10 = {}".format(number, sum_10))
