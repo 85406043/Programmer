931a34b291d8c185f257df629a8925962df2d48b
mac <85289658+85406043@users.noreply.github.com>
2026-03-02 10:49:04 -0300

    Update exe50.py

--- Estudying/Python/World 2/exe50.py
+++ Estudying/Python/World 2/exe50.py
@@ -1,13 +1,15 @@
 """Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o."""
-
-listNumbersPairs = []
-listNumbersOdd = []
-
+#variables
+listNumbersPairs = [] #list
+sum = 0
+#loop
 for integer in range(1, 7):
   number = int(input("Enter a number: "))
   if number % 2 == 0:
     listNumbersPairs.append(number)
+    sum += number
   else:
     pass
-
-print(f"Numbers Pairs: {", ".join(listNumbersPairs)}.")
+#Show result
+print(f"Numbers Pairs: {listNumbersPairs}")
+print(f"The sum between pairs: {sum}")
